69d2534a7bc30bea4c3cae2dfe1da619ee57fed6
Nanthunk <ksnandhakumarcgm@gmail.com>
2025-12-28 11:55:40 +0530

    Fix css import path

--- mern-project/backend/index.js
+++ mern-project/backend/index.js
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
+ 
 
 /* =======================
    🔥 EMPLOYEE TRACKING
--- mern-project/frontend/src/main.jsx
+++ mern-project/frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import "./styles/layout.css";
-import "./styles/dashboard.css";
+import "./styles/Layout.css";
+import "./styles/Dashboard.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
