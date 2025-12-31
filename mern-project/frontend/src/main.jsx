f57b2cdeffd2197f658d09caaeb812f186248dea
Nanthunk <ksnandhakumarcgm@gmail.com>
2025-12-31 17:46:36 +0530

    update fronto

--- mern-project/frontend/src/main.jsx
+++ mern-project/frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+
 
 import "./styles/Layout.css";
 import "./styles/Dashboard.css";
+
+axios.defaults.withCredentials = true;
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
